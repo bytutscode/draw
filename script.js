53c00ddaa2cd8df3075912e086d53af74f172de5
bytutscode <101745495+bytutscode@users.noreply.github.com>
2022-06-26 15:23:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let currentcolor = 'black';
+let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
+
+let screen = document.querySelector('#tela');
+let ctx = screen.getContext('2d');
+            // events
+screen.addEventListener('mousemove',mouseMoveEvent);
+screen.addEventListener('mousedown',mouseDownEvent);
+screen.addEventListener('mouseup',mouseUpEvent);
+document.querySelector('.clear').addEventListener('click', clearScreen);
+
+            //functions
+function mouseMoveEvent(e){
+    if(canDraw){
+        draw(e.pageX,e.pageY)
+    }
+}
+function mouseDownEvent(e){
+    canDraw = true;
+    mouseX = e.pageX - screen.offsetLeft;
+    mouseY = e.pageY - screen.offsetTop;
+}
+function mouseUpEvent(){
+    canDraw = false;
+}
+
+function draw(x,y){
+    let pointX = x - screen.offsetLeft;
+    let pointY = y - screen.offsetTop;
+
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineJoin = 'round';
+    ctx.moveTo(mouseX, mouseY);
+    ctx.lineTo(pointX, pointY);
+    ctx.closePath();
+    ctx.strokeStyle = currentcolor;
+    ctx.stroke();
+
+    mouseX = pointX;
+    mouseY = pointY;
+}
+
+document.querySelectorAll('.color').forEach(item =>{
+    item.addEventListener('click', clickItem);
+});
+
+function clickItem(e){
+    let color = e.target.getAttribute('data-color');
+    currentcolor = color;
+
+    document.querySelector('.color.active').classList.remove('active');
+    e.target.classList.add('active');
+}
+
+function clearScreen() {
+    ctx.setTransform(1,0,0,1,0,0);
+    ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height)
+}
